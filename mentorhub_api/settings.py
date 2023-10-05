4323501072fb133ae60b1185d39672b674b1c2a3
Abighe Pius <abighe-simon.pius@datasphir.com>
2023-10-05 12:09:37 +0100

    updated

--- mentorhub_api/settings.py
+++ mentorhub_api/settings.py
@@ -98,16 +98,18 @@ WSGI_APPLICATION = 'mentorhub_api.wsgi.application'
 # CORS WHITELIST
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "https://relaxed-curie-e9a516.netlify.app",
+    "fegor.pythonanywhere.com",
     "http://127.0.0.1:8080"
 ]
 
 CORS_ORIGIN_REGEX_WHITELIST = [
     r"^https://\w+\.netlify\.app$",
+    "fegor.pythonanywhere.com",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://loclahost",
+    "fegor.pythonanywhere.com",
 ]
 
 
