3f84114cd1f8ffa0340701d1ca6562fe8d455893
Abighe Pius <abighe-simon.pius@datasphir.com>
2023-10-08 08:08:35 +0100

    updated

--- mentorhub_api/settings.py
+++ mentorhub_api/settings.py
@@ -132,12 +132,12 @@ DATABASES = {
 # # Database
 # # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
