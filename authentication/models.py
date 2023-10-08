a06677e32e08598d4ad42b4df5ad0d7ff7fcba73
Abighe Pius <abighe-simon.pius@datasphir.com>
2023-10-08 08:07:01 +0100

    updated

--- authentication/models.py
+++ authentication/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("user_type", 'ADMIN')
+        extra_fields.setdefault("is_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("superuser must set is_staff to True"))
--- mentorhub_api/settings.py
+++ mentorhub_api/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-
-# ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['fegor.pythonanywhere.com']
 
 AUTH_USER_MODEL = 'authentication.User'
@@ -108,27 +107,27 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+    }
+}
 
 
 # # Test Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Fegor$mentorhub',
-#         'USER': 'Fegor',
-#         'PASSWORD': 'quv@)BX*Cioo',
-#         'HOST': 'Fegor.mysql.pythonanywhere-services.com',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Fegor$mentorhub',
+        'USER': 'Fegor',
+        'PASSWORD': 'quv@)BX*Cioo',
+        'HOST': 'Fegor.mysql.pythonanywhere-services.com',
+    }
+}
 
 # # Database
 # # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
